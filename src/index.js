fcdff1451da8360f7f8f5ed0a5a561933a647328
Prince Segbefia <princeasegbefia@gmail.com>
2021-10-19 21:51:44 -0600

    Endpoints setup

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+
+// Create new user
+
 app.post('/users', async (req, res) => {
     const user = new User(req.body)
 
@@ -18,6 +21,7 @@ app.post('/users', async (req, res) => {
         res.status(400).send(e)
     }
 })
+// Read all users
 
 app.get('/users', async (req, res) => {
 
@@ -28,6 +32,7 @@ app.get('/users', async (req, res) => {
         res.status(500).send()
     }
 })
+// Read user by id
 
 app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
@@ -43,6 +48,46 @@ app.get('/users/:id', async (req, res) => {
         res.status(500).send()
     }
 })
+// Update User by _id
+
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'age', 'email', 'password']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+// Delete user by _id
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+// Create new task
 
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
@@ -54,6 +99,8 @@ app.post('/tasks', async (req, res) => {
     }
 })
 
+// Read all tasks
+
 app.get('/tasks', async (req, res) => {
     try {
         const tasks = await Task.find({})
@@ -62,6 +109,8 @@ app.get('/tasks', async (req, res) => {
         res.status(500).send()
     }
 })
+
+// Read task by id
 
 app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id
@@ -75,6 +124,43 @@ app.get('/tasks/:id', async (req, res) => {
         res.send(task)
     } catch (e) {
         res.status(500).send()
+    }
+})
+
+// Update task by _id
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+// Delete task by _id
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send(e)
     }
 })
 
