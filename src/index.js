ddd9491d65ea8405149968bf169319ff29b5f1f6
Prince Segbefia <princeasegbefia@gmail.com>
2021-10-22 00:39:27 -0600

    Authentication

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if(req.method === 'GET'){
+//         res.send('Get requests are disabled')
+//     }
+//     else {
+//         next()
+//     }
+// })
+
+// app.use((req, res, next) => {
+//         res.status(503).send('Website is under maintenance')
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -17,3 +30,17 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port: ' + port)
 })
+
+
+// Example
+const jwt = require('jsonwebtoken')
+
+const myFunction = async () => {
+    const token = jwt.sign({ _id: '1245' }, 'thisismynewcourse', { expiresIn: '7 days' })
+    console.log(token)
+
+    const data = jwt.verify(token, 'thisismynewcourse')
+    console.log(data)
+}
+
+myFunction()
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const User = require('../models/user')
 const router = new express.Router()
+const auth =  require('../middleware/auth')
 
 // Create new user
 
@@ -9,22 +10,30 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({user, token})
     } catch (e) {
         res.status(400).send(e)
     }
 })
-// Read all users
 
-router.get('/users', async (req, res) => {
+// Create user login
 
+router.post('/users/login', async (req, res) => {
     try {
-        const users = await User.find({})
-        res.send(users)
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
     } catch (e) {
-        res.status(500).send()
+        res.status(400).send()
     }
 })
+// Read all users
+
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
+})
+
 // Read user by id
 
 router.get('/users/:id', async (req, res) => {
@@ -53,7 +62,11 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        const user = await User.findById(req.params.id)
+
+        updates.forEach((update) => user[update] = req.body[update])
+
+        await user.save()
 
         if (!user) {
             return res.status(404).send()
